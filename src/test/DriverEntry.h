d5ff73a3dca51911b1ede4a028a8c04231873fbb
correy <112426112@qq.com>
2023-10-04 07:20:46 +0800

    改进头文件。

--- src/test/DriverEntry.h
+++ src/test/DriverEntry.h
@@ -1,6 +1,87 @@
 #pragma once
 
 
+#if (NTDDI_VERSION >= NTDDI_VISTA)
+#define NDIS60 1
+#define NDIS_SUPPORT_NDIS6 1
+#endif 
+
+#define POOL_NX_OPTIN 1
+#define _CRT_NON_CONFORMING_SWPRINTFS
+#define INITGUID
+#define NTSTRSAFE_LIB
+
+#pragma warning(disable:4200) // 使用了非标准扩展 : 结构/联合中的零大小数组
+#pragma warning(disable:4201) // unnamed struct/union
+#pragma warning(disable:4214) // 使用了非标准扩展: 整形以外的位域类型
+#pragma warning(disable:4127) // 条件表达式是常量
+#pragma warning(disable:4057) // 在稍微不同的基类型间接寻址上不同
+#pragma warning(disable:4152) // 非标准扩展，表达式中的函数/数据指针转换
+#pragma warning(disable:28172) //The function 'XXX' has PAGED_CODE or PAGED_CODE_LOCKED but is not declared to be in a paged segment. 原因：1.函数内IRQL升级，2.函数内的函数的参数用局部变量，且要求这个变量是非分页内存。
+
+#include <winerror.h>
+#include <ntifs.h>
+#include <wdm.h>
+#include <ntddk.h>
+#include <windef.h> //应该放在ntddk.h的后面.
+#include <in6addr.h>
+#include <ip2string.h>
+#include <guiddef.h>
+#include <ndis.h>
+#include <initguid.h> //静态定义UUID用的，否则：error LNK2001。
+#include <Ntstrsafe.h>
+#include <ipmib.h>
+#include <netpnp.h>
+#include <ntintsafe.h>
+#include <fltkernel.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <Bcrypt.h>
+
+
+/*
+WDK7600.16385.1的内核头文件没有u_short的定义,用户层的头文件有u_short的定义.
+SOCKADDR结构里用到u_short.
+SOCKADDR在ws2def.h中定义.
+ws2def.h不建议直接包含.
+netioapi.h包含ws2def.h等文件.
+所以在WDK7600.16385.1中,如果不包含应用层的头文件,应该在包含netioapi.h之前,加上u_short的定义.
+否者,每个包含(包括间接包含)ws2def.h的c/cpp文件都出现一大堆的错误.
+*/
+typedef unsigned short  u_short;
+#include <netioapi.h>
+//#include <ws2def.h>
+#include <ws2ipdef.h>
+#include <mstcpip.h>
+#include <wmilib.h>
+#include <wmistr.h>
+#include <tdi.h>
+#include <tdiinfo.h>
+#include <tdikrnl.h>
+#include <tdistat.h>
+#include <fwpmk.h>
+#include <wsk.h>
+#include <ntimage.h>
+#include <fwpsk.h>  //NDIS61
+#include <dontuse.h>
+#include <suppress.h>
+#include <aux_klib.h>
+#include <assert.h>
+#include <Ntdddisk.h>
+#include <intrin.h> //VS2012编译。
+#include <immintrin.h>//VS2012编译。
+//#include <mmintrin.h> //WDK 编译。
+//#include <emmintrin.h>//WDK 编译。
+//#include <xmmintrin.h>//WDK 编译。
+#include <wdmsec.h>
+#define SECURITY_KERNEL
+#include <sspi.h>
+
+#define TAG 'tset' //test
+
+
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
 
